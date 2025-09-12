e0f47e287a96ad871a7a783beb4d4f6321a4f5ef
VICTOR <victor.cai@oocl.com>
2025-09-12 15:15:54 +0800

    refactor: extract TodoReducer

---
+++ src/reducers/TodoReducer.js
@@ -0,0 +1,15 @@
+export function todoReducer(state, action) {
+    switch (action.type) {
+        case "TOGGLE_TODO":
+            const newState = [...state]
+            const id = action.payload.id
+            return newState.map((value) => {
+                if (value.id === id) {
+                    return {id, text: value.text, done: !value.done}
+                }
+                return value
+            })
+        default:
+            return state
+    }
+}
