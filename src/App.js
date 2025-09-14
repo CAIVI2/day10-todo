7d5c84eba69ba5776fe34f46c3066a78c66a33fe
VICTOR <victor.cai@oocl.com>
2025-09-14 11:32:26 +0800

    refactor: rename text in initState

--- src/App.js
+++ src/App.js
@@ -5,8 +5,8 @@ import {TodoContext as TodoContext1} from "./contexts/TodoContext";
 import {MultipleTodd} from "./components/MultipleTodd";
 
 export const initState = [
-    {id: 1, text: "the first todo", done: false},
-    {id: 2, text: "the second todo", done: false},
+    {id: 1, text: "This is the first thing I need to do", done: false},
+    {id: 2, text: "This is the second thing I need to do", done: false},
 ];
 
 function App() {
