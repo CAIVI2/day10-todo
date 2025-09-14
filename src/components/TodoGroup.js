7dbd233072b0e95e7f3a1851da125dfb85dfd49d
VICTOR <victor.cai@oocl.com>
2025-09-14 10:29:01 +0800

    feat: add tips for empty data

--- src/components/TodoGroup.js
+++ src/components/TodoGroup.js
@@ -7,13 +7,15 @@ export function TodoGroup() {
     const {state, dispatch} = useContext(TodoContext)
     return <div>
         <div className={"todo-title"}>Todo List</div>
-        {
-            state.map((item, index) => {
-                return <div className="todo-row" key={index}>
-                    <TodoItem todo={item} key={index} index={index}/>
+        {state.length === 0 ? (
+            <div className="todo-empty">Add the things you need to do today...</div>
+        ) : (
+            state.map((item, index) => (
+                <div className="todo-row" key={index}>
+                    <TodoItem todo={item} index={index}/>
                     <button type="button">X</button>
                 </div>
-            })
-        }
+            ))
+        )}
     </div>;
 }
