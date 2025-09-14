16797a5152fa144a9e4da4dfd3d581663c31e783
VICTOR <victor.cai@oocl.com>
2025-09-14 10:44:12 +0800

    feat: delete todo

--- src/components/TodoGroup.js
+++ src/components/TodoGroup.js
@@ -5,6 +5,14 @@ import {TodoContext} from "../contexts/TodoContext";
 
 export function TodoGroup() {
     const {state, dispatch} = useContext(TodoContext)
+
+    function deleteToto(item) {
+        dispatch({
+            type: "DELETE_TODO",
+            payload: {id: item.id}
+        })
+    }
+
     return <div>
         <div className={"todo-title"}>Todo List</div>
         {state.length === 0 ? (
@@ -13,7 +21,7 @@ export function TodoGroup() {
             state.map((item, index) => (
                 <div className="todo-row" key={index}>
                     <TodoItem todo={item} index={index}/>
-                    <button type="button">X</button>
+                    <button type="button" onClick={() => deleteToto(item)}>X</button>
                 </div>
             ))
         )}
--- src/reducers/TodoReducer.js
+++ src/reducers/TodoReducer.js
@@ -1,14 +1,16 @@
 export function todoReducer(state, action) {
+    const id = action.payload.id
     switch (action.type) {
         case "TOGGLE_TODO":
             const newState = [...state]
-            const id = action.payload.id
             return newState.map((value) => {
                 if (value.id === id) {
                     return {id, text: value.text, done: !value.done}
                 }
                 return value
             })
+        case "DELETE_TODO":
+            return state.filter((value) => value.id !== id);
         default:
             return state
     }
