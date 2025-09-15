18a18d4f365bfd9b5a00330a3350b59de87b90c3
VICTOR <victor.cai@oocl.com>
2025-09-15 14:14:42 +0800

    refactor: remove initState to TodoContext

--- src/App.js
+++ src/App.js
@@ -1,14 +1,9 @@
 import {useReducer} from "react";
 import "./App.css"
 import {todoReducer} from "./reducers/TodoReducer";
-import {TodoContext} from "./contexts/TodoContext";
+import {initState, TodoContext} from "./contexts/TodoContext";
 import {RouterProvider} from "react-router";
 import {routes} from "./routes/Routes";
-
-export const initState = [
-    {id: 1, text: "This is the first thing I need to do", done: false},
-    {id: 2, text: "This is the second thing I need to do", done: false},
-];
 
 function App() {
     const [state, dispatch] = useReducer(todoReducer, initState);
