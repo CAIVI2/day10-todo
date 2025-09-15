6c585c9e998ca902cd900f58b96e8959fd148b6c
VICTOR <victor.cai@oocl.com>
2025-09-15 11:04:47 +0800

    feat: add todos/{id} route

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import {useReducer} from "react";
+import {useContext, useReducer} from "react";
 import "./App.css"
 import {todoReducer} from "./reducers/TodoReducer";
-import {TodoContext as TodoContext1} from "./contexts/TodoContext";
+import {TodoContext} from "./contexts/TodoContext";
 import {TodoList} from "./components/TodoList";
-import {createBrowserRouter, NavLink, Outlet, RouterProvider, useRouteError} from "react-router";
+import {createBrowserRouter, NavLink, Outlet, RouterProvider, useParams, useRouteError} from "react-router";
+import {TodoItem} from "./components/TodoItem";
 
 export const initState = [
     {id: 1, text: "This is the first thing I need to do", done: false},
@@ -16,6 +17,7 @@ function DefaultLayout() {
             <nav>
                 <ul>
                     <li><NavLink to={"/"}>Home</NavLink></li>
+                    <li><NavLink to={"/todos/1"}>ID:1</NavLink></li>
                 </ul>
             </nav>
         </header>
@@ -28,19 +30,43 @@ function DefaultLayout() {
 function ErrorPage() {
     const error = useRouteError();
     return <div>
-        {error.status === 404 ? <div className={"not-found"}><h1>404 not found</h1><span>Try</span></div> :
+        {error?.status === 404 ? <div className={"not-found"}><h1>404 not found</h1><span>Try</span></div> :
             <div>{JSON.stringify(error)}</div>}
+    </div>
+}
+
+function HomePage() {
+    return <TodoList/>;
+}
+
+function TodoDetailPage() {
+    const {id} = useParams();
+    const {state, dispatch} = useContext(TodoContext);
+    const todo = state.filter((todo) => todo.id === parseInt(id));
+
+    if (todo.length === 0) {
+        return <div>Not Found Todo</div>
+    }
+
+    return <div>
+        {JSON.stringify(todo)}
+        <TodoItem todo={todo[0]} index={id}></TodoItem>
     </div>
 }
 
 const routes = createBrowserRouter([
     {
         path: "/",
-        element: <ErrorPage/>,
+        element: <DefaultLayout/>,
+        errorElement: <ErrorPage/>,
         children: [
             {
                 path: "/",
-                element: <TodoList/>
+                element: <HomePage/>
+            },
+            {
+                path: "/todos/:id",
+                element: <TodoDetailPage/>
             }
         ]
     }
@@ -50,9 +76,9 @@ function App() {
     const [state, dispatch] = useReducer(todoReducer, initState);
     return (
         <div>
-            <TodoContext1.Provider value={{state, dispatch}}>
+            <TodoContext.Provider value={{state, dispatch}}>
                 <RouterProvider router={routes}></RouterProvider>
-            </TodoContext1.Provider>
+            </TodoContext.Provider>
         </div>
     );
 }
