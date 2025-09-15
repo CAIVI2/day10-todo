63aed1e4b980fbd404352f71a0a2159b2401a883
VICTOR <victor.cai@oocl.com>
2025-09-15 10:35:40 +0800

    feat: add router

--- src/App.js
+++ src/App.js
@@ -3,18 +3,26 @@ import "./App.css"
 import {todoReducer} from "./reducers/TodoReducer";
 import {TodoContext as TodoContext1} from "./contexts/TodoContext";
 import {TodoList} from "./components/TodoList";
+import {createBrowserRouter, RouterProvider} from "react-router";
 
 export const initState = [
     {id: 1, text: "This is the first thing I need to do", done: false},
     {id: 2, text: "This is the second thing I need to do", done: false},
 ];
 
+const routes = createBrowserRouter([
+    {
+        path: "/",
+        element: <TodoList/>
+    }
+]);
+
 function App() {
     const [state, dispatch] = useReducer(todoReducer, initState);
     return (
         <div>
             <TodoContext1 value={{state, dispatch}}>
-                <TodoList/>
+                <RouterProvider router={routes}></RouterProvider>
             </TodoContext1>
         </div>
     );
