0ac86cb8c9182234acc99ae87dede643480df8ab
VICTOR <victor.cai@oocl.com>
2025-09-15 14:16:35 +0800

    feat: use axios

--- src/App.js
+++ src/App.js
@@ -4,6 +4,13 @@ import {todoReducer} from "./reducers/TodoReducer";
 import {initState, TodoContext} from "./contexts/TodoContext";
 import {RouterProvider} from "react-router";
 import {routes} from "./routes/Routes";
+import axios from "axios";
+
+const api = axios.create({
+    baseURL: "https://68c7ac8d5d8d9f5147328728.mockapi.io/",
+    headers: {'Content-Type': 'application/json'},
+    timeout: 10_000
+})
 
 function App() {
     const [state, dispatch] = useReducer(todoReducer, initState);
