c2cdb3731addfd90386482e025747617dccdbded
VICTOR <victor.cai@oocl.com>
2025-09-15 11:37:04 +0800

    feat: show done todos

---
+++ src/pages/DoneListPage.js
@@ -0,0 +1,18 @@
+import {useContext} from "react";
+import {TodoContext} from "../contexts/TodoContext";
+import {TodoItem} from "../components/TodoItem";
+
+export function DoneListPage() {
+    const {state} = useContext(TodoContext)
+    const doneTodos = state.filter(item => item.done);
+
+    return <div>
+        {doneTodos.length > 0 ? (
+            doneTodos.map((item, index) => {
+                return <TodoItem todo={item} index={index}/>
+            })
+        ) : (
+            <p>No done things...</p>
+        )}
+    </div>;
+}
