7cffc6dc5fc7c786f7af485bc222621e51ab00d2
VICTOR <victor.cai@oocl.com>
2025-09-16 09:49:35 +0800

    refactor: add css style

--- src/pages/DoneListPage.js
+++ src/pages/DoneListPage.js
@@ -1,19 +1,29 @@
 import {useContext} from "react";
 import {TodoContext} from "../contexts/TodoContext";
 import {TodoItem} from "../components/TodoItem";
+import {List, Empty, Typography} from 'antd';
+
+const {Title} = Typography;
 
 export function DoneListPage() {
     const {state} = useContext(TodoContext)
     const doneTodos = state.filter(item => item.done);
 
-    return <div>
-        <div className={"title"}>Done List</div>
-        {doneTodos.length > 0 ? (
-            doneTodos.map((item, index) => {
-                return <TodoItem todo={item} index={index}/>
-            })
-        ) : (
-            <p>No done things...</p>
-        )}
+    return <div className="todo-group-container">
+        <div className="todo-list-card">
+            <Title level={4} className="todo-list-title">Done List</Title>
+            <List
+                dataSource={doneTodos}
+                locale={{
+                    emptyText: <Empty className="todo-list-empty"
+                                      description={"No done things..."}/>
+                }}
+                renderItem={(item, index) => (
+                    <List.Item key={item.id} className="todo-list-item">
+                        <TodoItem todo={item} index={index}/>
+                    </List.Item>
+                )}
+            />
+        </div>
     </div>;
 }
