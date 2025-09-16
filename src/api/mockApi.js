bfed72ae5a7841ae489b91ed51ac193346a0a7b5
VICTOR <victor.cai@oocl.com>
2025-09-16 10:25:04 +0800

    feat: add interceptors

--- src/api/mockApi.js
+++ src/api/mockApi.js
@@ -1,7 +1,27 @@
 import axios from "axios";
+import {message} from "antd";
 
-export const api = axios.create({
+const api = axios.create({
     baseURL: "https://68c7ac8d5d8d9f5147328728.mockapi.io/",
     headers: {'Content-Type': 'application/json'},
     timeout: 10_000
 })
+
+api.interceptors.response.use(
+    (response) => {
+        return response;
+    },
+    (error) => {
+        const {status} = error.response;
+        if (status === 404) {
+            // alert(error.message);
+            message
+                .error(error.message, 1_1000)
+                .then(r => {})
+        }
+
+        return Promise.reject(error);
+    }
+);
+
+export {api}
